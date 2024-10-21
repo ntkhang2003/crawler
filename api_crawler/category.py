50782c8648be244afaa274d46793cd8ced9304b4
ntkhang2003 <ntkhang2003@gmail.com>
2024-10-21 23:16:11 +0700

    update cate

--- api_crawler/category.py
+++ api_crawler/category.py
@@ -3,41 +3,43 @@ import json
 
 url = "https://papi.fptshop.com.vn/gw/v1/public/fulltext-search-service/category"
 
-skipCount = 0
-maxResultCount = 50
+category = ['dien-thoai', 'may-tinh-xach-tay']
 
-payload = json.dumps({
-    "skipCount": skipCount,
-    "maxResultCount": maxResultCount,
-    "sortMethod": "noi-bat",
-    "slug": "dien-thoai",
-    "categoryType": "category"
-})
-
-headers = {
-	'Content-Type': 'application/json'
-}
-
-response = requests.request("POST", url, headers=headers, data=payload)
-
-data = json.loads(response.text)
-
-totalCount = data.get('totalCount')
-
-while (skipCount + maxResultCount) < totalCount:
-    skipCount += maxResultCount
+for cat in category:
+    skipCount = 0
+    maxResultCount = 50
     payload = json.dumps({
         "skipCount": skipCount,
         "maxResultCount": maxResultCount,
         "sortMethod": "noi-bat",
-        "slug": "dien-thoai",
+        "slug": cat,
         "categoryType": "category"
     })
+
+    headers = {
+        'Content-Type': 'application/json'
+    }
+
     response = requests.request("POST", url, headers=headers, data=payload)
-    reponse_json = json.loads(response.text)
-    data['items'].extend(reponse_json['items'])
 
-print('Number of items cralwed: ', len(data['items']), '/', totalCount)
+    data = json.loads(response.text)
 
-with open('category.json', 'w', encoding='utf-8') as f:
-    json.dump(data, f, ensure_ascii=False, indent=4)
+    totalCount = data.get('totalCount')
+
+    while (skipCount + maxResultCount) < totalCount:
+        skipCount += maxResultCount
+        payload = json.dumps({
+            "skipCount": skipCount,
+            "maxResultCount": maxResultCount,
+            "sortMethod": "noi-bat",
+            "slug": cat,
+            "categoryType": "category"
+        })
+        response = requests.request("POST", url, headers=headers, data=payload)
+        reponse_json = json.loads(response.text)
+        data['items'].extend(reponse_json.get('items'))
+
+    print(f'Number of items {cat} cralwed: ', len(data['items']), '/', totalCount)
+
+    with open(f'{cat}.json', 'w', encoding='utf-8') as f:
+        json.dump(data, f, ensure_ascii=False, indent=4)
